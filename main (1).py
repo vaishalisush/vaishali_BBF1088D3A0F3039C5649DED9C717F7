67899d28a625cf62f92a5e1f9f453374f9d27633
vaishalisush <144663163+vaishalisush@users.noreply.github.com>
2023-10-21 19:03:48 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,7 +1,22 @@
-def factorial(x):
-  if x == 1:
-    return 1
-  else:
-    return x * factorial(x - 1)
-num = 3
-print("The factorial of", num, "is", factorial(num))
+# Define the Player class
+class Player:
+    def play(self):
+        print("The player is playing cricket.")
+
+# Define the Batsman class, derived from Player
+class Batsman(Player):
+    def play(self):
+        print("The batsman is batting.")
+
+# Define the Bowler class, derived from Player
+class Bowler(Player):
+    def play(self):
+        print("The bowler is bowling.")
+
+# Create objects of Batsman and Bowler classes
+batsman = Batsman()
+bowler = Bowler()
+
+# Call the play() method for each object
+batsman.play()
+bowler.play()
--- main.py
+++ main.py
@@ -1,5 +1,32 @@
-year = int(input("Enter year to be checked"))
-if (year % 4 == 0 and year % 100 != 0 or year % 400 == 0):
-  print("The year is a leap year!")
-else:
-  print("The year isn't a leap year!")
+# Python program to create Bankaccount class
+# with both a deposit() and a withdraw() function
+class Bank_Account:
+    def __init__(self):
+        self.balance=0
+        print("Hello!!! Welcome to the Deposit & Withdrawal Machine")
+ 
+    def deposit(self):
+        amount=float(input("Enter amount to be Deposited: "))
+        self.balance += amount
+        print("\n Amount Deposited:",amount)
+ 
+    def withdraw(self):
+        amount = float(input("Enter amount to be Withdrawn: "))
+        if self.balance>=amount:
+            self.balance-=amount
+            print("\n You Withdrew:", amount)
+        else:
+            print("\n Insufficient balance  ")
+ 
+    def display(self):
+        print("\n Net Available Balance=",self.balance)
+ 
+# Driver code
+  
+# creating an object of class
+s = Bank_Account()
+  
+# Calling functions with that class object
+s.deposit()
+s.withdraw()
+s.display()
